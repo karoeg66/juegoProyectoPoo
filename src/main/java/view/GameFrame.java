003c3cc73518e13df88df95a6edc34306f58a347
karo.eg66 <luedk.garcia@autonoma.edu.co>
2026-05-20 23:16:35 -0500

    Documentacion de game frame

--- src/main/java/view/GameFrame.java
+++ src/main/java/view/GameFrame.java
@@ -2,13 +2,30 @@ package view;
 
 import javax.swing.*;
 
+/**
+ * Ventana principal del juego (JFrame).
+ * Se encarga de inicializar el contenedor principal, configurar las propiedades
+ * basicas de la ventana (titulo, dimensiones, cierre) y alojar el panel del juego.
+ */
+
+
 public class GameFrame extends JFrame {
 
     private GamePanel gamePanel;
 
+    /**
+     * Constructor del marco de la ventana.
+     * Llama al metodo de inicializacion para configurar los componentes.
+     */
+
     public GameFrame() {
         iniciarVentana();
     }
+
+    /**
+     * Configura los parametros de la ventana, añade el panel del juego,
+     * empaqueta los componentes, centra la ventana en la pantalla y le da foco.
+     */
 
     public void iniciarVentana() {
         setTitle("One Piece - Enemies in sight");
@@ -24,6 +41,11 @@ public class GameFrame extends JFrame {
 
         gamePanel.requestFocusInWindow();
     }
+
+    /**
+     * Obtiene el panel de juego asociado a esta ventana.
+     * @return El objeto GamePanel que se esta ejecutando
+     */
 
     public GamePanel getGamePanel() {
         return gamePanel;
