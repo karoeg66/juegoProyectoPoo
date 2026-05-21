1edfeb16247ee5a58427029a38b8287a25067ebd
karo.eg66 <luedk.garcia@autonoma.edu.co>
2026-05-20 23:22:48 -0500

    Documentacion de pantalla nombre

--- src/main/java/view/PantallaNombre.java
+++ src/main/java/view/PantallaNombre.java
@@ -3,11 +3,29 @@ package view;
 import javax.swing.*;
 import java.awt.*;
 
+
+/**
+ * Clase que representa la interfaz grafica para que el usuario introduzca su nombre.
+ * Define las areas interactivas para la caja de texto y el boton de confirmacion,
+ * y se encarga de renderizar de manera personalizada estos elementos en pantalla.
+ */
+
+
 public class PantallaNombre extends JPanel {
 
         public Rectangle inputBox = new Rectangle(440, 300, 400, 50);
         public Rectangle btnContinuar = new Rectangle(515, 390, 250, 50);
 
+    /**
+     * Dibuja los componentes visuales de la pantalla de ingreso de nombre,
+     * como el fondo, el titulo, la caja de texto con el nombre actual y el boton continuar.
+     * @param g Objeto Graphics para renderizar en el panel
+     * @param w Ancho actual del panel de juego
+     * @param h Alto actual del panel de juego
+     * @param nombreActual El texto del nombre que el usuario va escribiendo en tiempo real
+     */
+
+    
         public void draw(Graphics g, int w, int h, String nombreActual) {
             g.setColor(new Color(10, 20, 60));
             g.fillRect(0, 0, w, h);
